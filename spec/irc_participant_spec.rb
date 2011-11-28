85407be80c4c0556c4038e505940c6966e830c54
Kit Plummer <kitplummer@gmail.com>
2011-11-28 15:06:57 -0700

    copying files from luCEE.

---
+++ spec/irc_participant_spec.rb
@@ -0,0 +1,81 @@
+# Copyright 2011© MaestroDev.  All rights reserved.
+
+require 'spec_helper'
+
+describe Maestro::IrcParticipant do
+
+  describe 'consume()' do
+    before(:all) do
+
+      @test_participant = Maestro::IrcParticipant.new
+
+    end
+
+    after(:all) do
+    end
+
+    it 'should say a message' do
+      #fields = { 'body' => 'adios terd nugget, your composition is complete'}
+      wi = Ruote::Workitem.new({"fields" => {"body" => "testing"}, "params" => {"composition_task_id" => 1}})
+      bot = mock()
+      Maestro::Irc.stubs(:bot => bot)
+      bot.stubs(:start => true)
+
+      bot.stubs(:quit => true)
+      #channel = mock(:channel)
+      #channel.stubs(:say => true)
+      bot.stubs(:message)
+
+      @test_participant.stubs(:reply_to_engine)
+      
+      @test_participant.consume(wi)
+
+      wi.fields['__error__'].should eql('')
+    end
+
+    it 'should throw execption with an IRC connection error' do
+      wi = Ruote::Workitem.new({"fields" => {"body" => "testing"}})
+      bot = stub_everything
+      Maestro::Irc.stubs(:bot).returns bot
+
+      Maestro::ReceiveError.stubs(:new => Exception.new("an exceptions"))
+
+      context = mock()
+      error_handler = mock()
+      error_handler.stubs(:action_handle)
+      context.stubs(:error_handler => error_handler)
+      @test_participant.stubs(:current_context => context)
+
+
+      bot.stubs(:message).raises(RuntimeError)
+      @test_participant.stubs(:reply_to_engine)
+      @test_participant.consume(wi)
+      wi.fields['__error__'].should include('Failed to shout to IRC')
+    end
+
+    describe 'template' do
+
+      it 'should create body from template' do
+        composition = Composition.create(:name => "Template Test", :description => "Template Test")
+        run = Run.create(:composition_id => composition[:id], :name => "Some Run", :source => {}.to_json, :trigger_type => "manual")
+        wi = Ruote::Workitem.new({"fields" => {"composition_id" => composition[:id],"body" => "testing", "run_id" => run[:id],
+           "url" => File.join(File.dirname(__FILE__), "../../lib/maestro/part/templates/irc.txt.erb")}, "params" => {"composition_task_id" => 1}})
+        bot = mock()
+        Maestro::Irc.stubs(:bot => bot)
+        bot.stubs(:start => true)
+
+        bot.stubs(:quit => true)
+
+        bot.stubs(:message)
+
+        @test_participant.stubs(:reply_to_engine)
+      
+        @test_participant.consume(wi)
+
+        wi.fields['output'].should contain('[Template Test] Manually Executed')
+        wi.fields['__error__'].should eql('')
+      end
+
+    end
+  end
+end
---
+++ src/irc.rb
@@ -0,0 +1,101 @@
+# Copyright 2011© MaestroDev.  All rights reserved.
+
+require 'uuid'
+
+module Maestro
+  class Irc
+    
+    @@irc = nil
+    @@connected = false
+    @@chan = nil
+    
+    @bot = nil
+    
+    def initialize
+      @@connected = false
+      
+      @semaphore = Mutex.new
+
+      Thread.new do
+        @bot = Isaac::Bot.new do
+
+          configure do |c|
+          
+            config = Maestro.irc_config
+            # create a "random" nick so we don't have to worry about handling names
+            c.nick = config['nick'] + "-" +UUID.new.generate
+            c.server = config['host']
+            c.password = config['pass']
+            c.ssl = config['ssl']
+            c.port = config['port']
+          
+          end
+
+          on :connect do
+            @@chan = Maestro.irc_config['chan']
+            join @@chan
+            @@connected = true
+          end
+          
+          on :private do
+            puts 'message!'
+          end
+          
+        end 
+        
+        
+        begin
+          @bot.start
+        rescue Exception
+          Maestro.log.info "Closed IRC connection to #{@bot.host}"
+        end  
+        
+      end
+    end
+    
+    def connected?
+      @@connected
+    end
+    
+    def message(what_should_i_say)
+      begin
+        thread = Thread.new do
+          while !connected?
+            sleep(4)
+          end
+
+          @semaphore.synchronize do
+              @bot.msg @@chan, what_should_i_say
+              # sleep(2)
+          end
+        end
+        thread.join
+      rescue Exception
+        Maestro.log.error "Error sending message to IRC"
+      end
+    end    
+    
+    class << self
+      def bot
+        @@irc = Irc.new if @@irc.nil?
+        
+        @@irc
+      end
+              
+    end
+  end
+  
+  class IrcMessage
+    class << self
+      
+      def set_body(body = "I am an IRC message")
+        @@body = body
+      end
+      
+      def get_body
+        @@body
+      end
+            
+    end
+  end
+end
