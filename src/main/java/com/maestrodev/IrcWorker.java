6affafff68d262a8e74d6a8dfaf935752b93c50e
kellyp <kellyplummer@mac.com>
2011-12-21 08:54:44 -0800

    added java based irc plugin

---
+++ src/main/java/com/maestrodev/IrcWorker.java
@@ -0,0 +1,144 @@
+package com.maestrodev;
+
+import java.io.IOException;
+import org.schwering.irc.lib.IRCConnection;
+import org.schwering.irc.lib.IRCEventListener;
+import org.schwering.irc.lib.IRCModeParser;
+import org.schwering.irc.lib.IRCUser;
+
+/**
+ * Hello world!
+ *
+ */
+public class IrcWorker extends MaestroWorker
+{
+    
+    private boolean messageSent;
+    
+    public IrcWorker(){     
+        super();
+    }
+    
+    public void postMessage() throws Exception {
+        
+        final IRCConnection conn = new IRCConnection(
+                                    getField("server"), 
+                                    Integer.parseInt(getField("port").toString()),
+                                    Integer.parseInt(getField("port").toString()) + 1, 
+                                    null, 
+                                    getField("nickname"), 
+                                    getField("nickname"), 
+                                    getField("nickname")
+                                  ); 
+         conn.addIRCEventListener(new IrcEventListener(conn));
+         conn.setDaemon(true);
+         conn.setColors(false); 
+         conn.setPong(true); 
+         
+//         new Thread(){
+//         public void run(){
+             try {
+               conn.connect(); // Try to connect!!! Don't forget this!!!
+               messageSent = false;               
+             } catch (Exception ioexc) {
+               ioexc.printStackTrace(); 
+             }
+//            }
+//         }.start();
+         
+         while(!messageSent){
+            Thread.sleep(5000);
+         }
+    }    
+    
+    private class IrcEventListener implements IRCEventListener {
+
+        private IRCConnection connection;
+        
+        public IrcEventListener(IRCConnection connection){
+            this.connection = connection;
+        }
+            
+        public void onRegistered() {
+            System.err.println("connected");
+            this.connection.doJoin(getField("channel"));
+        }
+
+        public void onDisconnected() {
+            
+        }
+
+        public void onError(String string) {
+            
+        }
+
+        public void onError(int i, String string) {
+            
+        }
+
+        public void onInvite(String string, IRCUser ircu, String string1) {
+            
+        }
+
+        public void onJoin(String string, IRCUser ircu) {
+            System.out.println("Joined " + string);
+            this.connection.doPrivmsg(getField("channel"), getField("body"));
+            messageSent = true;
+        }
+
+        public void onKick(String string, IRCUser ircu, String string1, String string2) {
+           
+        }
+
+        public void onMode(String string, IRCUser ircu, IRCModeParser ircmp) {
+           
+        }
+
+        public void onMode(IRCUser ircu, String string, String string1) {
+           
+        }
+
+        public void onNick(IRCUser ircu, String string) {
+           
+        }
+
+        public void onNotice(String string, IRCUser ircu, String string1) {
+           
+        }
+
+        public void onPart(String string, IRCUser ircu, String string1) {
+           
+        }
+
+        public void onPing(String string) {
+           
+        }
+
+        public void onPrivmsg(String string, IRCUser ircu, String string1) {
+           
+        }
+
+        public void onQuit(IRCUser ircu, String string) {
+           
+        }
+
+        public void onReply(int i, String string, String string1) {
+           
+        }
+
+        public void onTopic(String string, IRCUser ircu, String string1) {
+           
+        }
+
+        public void unknown(String string, String string1, String string2, String string3) {
+           
+        }
+        
+    }
+  
+}
+
+
+    
+            
+
---
+++ src/test/java/com/maestrodev/IrcWorkerTest.java
@@ -0,0 +1,76 @@
+package com.maestrodev;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.json.simple.JSONObject;
+
+/**
+ * Unit test for simple App.
+ */
+public class IrcWorkerTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public IrcWorkerTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( IrcWorkerTest.class );
+    }
+//     {"body" : {"value" : "", "type" : "String", "required" : "true"},
+//		"nickname":{"value" : "", "type" : "String", "required" : "true"},
+//		"server":{"value" : "", "type" : "String", "required" : "true"},
+//		"password":{"value" : "", "type" : "Password", "required" : "false"},
+//		"ssl":{"value" : "", "type" : "Boolean", "required" : "true"},
+//		"port":{"value" : "", "type" : "Integer", "required" : "true"},
+//		"channel":{"value" : "", "type" : "String", "required" : "true"}
+
+    /**
+     * Test IrcWorker
+     */
+    public void testIrcWorker() throws NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException
+    {
+        IrcWorker ircWorker = new IrcWorker();
+        JSONObject fields = new JSONObject();
+        fields.put("body", "Hello From Javaland!");
+        fields.put("nickname", "irc-plugin-test");        
+        fields.put("server", "irc.freenode.net");
+        fields.put("password", null);
+        fields.put("ssl", "false");
+        fields.put("port", "6667");
+        fields.put("channel", "#kittest");        
+        
+        JSONObject workitem = new JSONObject();
+        workitem.put("fields", fields);
+        ircWorker.setWorkitem(workitem);
+        
+        HashMap stompConfig = new HashMap();
+        stompConfig.put("host", "localhost");
+        stompConfig.put("port", "61619");
+        stompConfig.put("queue", "/queue/test");
+        
+        ircWorker.setStompConfig(stompConfig);
+        
+        for(Object key : ircWorker.getWorkitem().keySet()){
+            System.out.println(key);
+        }
+        
+        Method method = ircWorker.getClass().getMethod("postMessage");
+        method.invoke(ircWorker);
+        
+    }
+}
